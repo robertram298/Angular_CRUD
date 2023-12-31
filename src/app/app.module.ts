6f1a600467d7878c8e1fd13e84949934c9495d89
robertram298@gmail.com <robertram298@gmail.com>
2023-12-31 18:08:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    UsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
